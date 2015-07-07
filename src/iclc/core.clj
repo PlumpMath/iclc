6e4ecee37d0fa344e1aab1b1ffcdb9ee933a15ea
kaosbeat <kasper.jordaens@gmail.com>
2015-07-07 22:32:05 +0200

    b-movie fix

--- src/iclc/core.clj
+++ src/iclc/core.clj
@@ -2,7 +2,31 @@
   (:use [overtone.core])
   (:require [quil.core :as q]
             [quil.middleware :as m]
-            [bohrbug.core :as b]
-            [kaos.core :as k])
-
+            )
   )
+
+;; 0 - setup environment
+(def superServerIP "localhost")
+(def superServerPort 57110)
+
+
+;; 1 - connect to supercollider
+(defonce superServer ( connect-external-server superServerIP  superServerPort))
+
+
+
+;; 2 - define & start central bpm-counter
+(defsynth bpm [bpm 120]
+  (let  [a   (/ bpm 120)
+          _ (tap:kr :step 60 (lf-saw:kr a))]
+     (out 100 (lf-saw:kr a)))) ;; put the control synths on channel 100 & on
+
+(def tempbeat (bpm 60))
+
+(defn mod16 []
+  (int (* 16 (/ (+ @(get-in tempbeat [:taps :step]) 1) 2 ))))
+
+(defn mod4 []
+
+
+  (int (/ (mod16) 4)))
--- src/kaos/core.clj
+++ src/kaos/core.clj
@@ -1,6 +1,56 @@
 (ns kaos.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [iclc.core :refer :all])
   (:use [clojure.core.match :only [match]])
-  (:require [polynome.core :as poly])
-)
+  (:require [polynome.core :as poly]))
 
-(def m (poly/init "/dev/tty.usbserial-m128-121"))
+
+
+
+;  _ __ ___   ___  _ __   ___  _ __ ___   ___
+; | '_ ` _ \ / _ \| '_ \ / _ \| '_ ` _ \ / _
+; | | | | | | (_) | | | | (_) | | | | | |  __/
+; |_| |_| |_|\___/|_| |_|\___/|_| |_| |_|\___|
+
+
+(defonce m (poly/init "/dev/tty.usbserial-m128-121"))
+
+
+(def width 1440)
+(def height 980)
+
+
+ ;; 4 -  start gfxcore
+
+;;start gfxcore, update state
+(defn setup []
+  (q/frame-rate 30)
+)
+(defn update [state]
+  {
+  :mod16 (mod16)
+   }
+  ;; (updateM)
+  )
+
+
+
+(defn draw [state]
+  (q/background 255 255 0)
+  (q/with-translation [ (+ 100 (* 10 (mod16))) 100 0]
+    (q/box 100))
+ )
+
+(q/defsketch halic
+  :title "halic"
+;;  :size :fullscreen
+  :size [width height]
+  ;;:features [:present]
+  :setup setup
+  :update update
+  :draw draw
+  :renderer :p3d
+  :middleware [m/fun-mode]
+
+  )
