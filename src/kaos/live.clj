b4cc6f2faa66f026f23826a8d16e41fb228a4d30
kaosbeat <kasper.jordaens@gmail.com>
2015-07-14 00:06:42 +0100

    this works

--- src/kaos/live.clj
+++ src/kaos/live.clj
@@ -1,12 +1,45 @@
-(ns kaos.live
-  (:require [iclc.core :refer :all]
-            [kaos.core]))
+(ns iclc.core
+  (:require [newtonian.kaosnewt :as kn])
+  )
+
+(defn draw-particle [{:keys [position velocity accel]} state]
+  (let [x (:x position)
+        y (:y position)]
+
+;    (println position)
+    (q/with-translation [ x y]
+      (q/box 20))
+    )
+  )
+
+
+(defn drawP [state]
+  (let [particles @newt/particles]
+    (doseq [p particles]
+      (draw-particle p state))
+    ))
 
 
 
-(def test 12)
+(defn draw [state]
+  (q/background 255 128 0)
+  (drawP state)
 
-(definst fook [] (saw 220))
 
-(foo)
-(stop)
+;  (dotimes [n 8] (kn/changespeed n 0 0))
+  ;(kn/changespeed (mod @bbeat 8) 0 10)
+  (q/box 50)
+  )
+
+(kn/addemitter 100 100 100 0)
+(kn/changespeed 0 0 0)
+(@newt/emitters 0)
+(kn/changeemitspeed 0 1)
+
+(dotimes [n 8]
+  (kn/addemitter (* (/ width 8) (+ 1 n)) 100 0 10 )
+  )
+
+
+
+(swap! newt/emitters pop)
